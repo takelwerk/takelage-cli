5e2ff1639a2b82130ef86519a202116c9e785a6d
Stephan Wehrheim <swehrheim@geospin.de>
2020-03-26 13:47:50 +0100

    Fix copy&paste error

--- features/lib/container.rb
+++ features/lib/container.rb
@@ -3,10 +3,10 @@ def stop_mock_container
       'docker stop takelage-mock_cucumber ' +
       '&> /dev/null'
   cmd_stop_mock_container_finite = 'tau-cli docker container check existing takelage-mock_finite && ' +
-      'docker stop takelage-mock_cucumber ' +
+      'docker stop takelage-mock_finite ' +
       '&> /dev/null'
   cmd_stop_mock_container_infinite = 'tau-cli docker container check existing takelage-mock_infinite && ' +
-      'docker stop takelage-mock_cucumber ' +
+      'docker stop takelage-mock_infinite ' +
       '&> /dev/null'
   system cmd_stop_mock_container_cucumber
   system cmd_stop_mock_container_finite
