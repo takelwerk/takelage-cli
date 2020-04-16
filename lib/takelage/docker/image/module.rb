995afc3b0d771b69f15894d3fa1694bae9de9d5f
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-16 18:01:41 +0200

    First try to pull latest docker image

--- lib/takelage/docker/image/module.rb
+++ lib/takelage/docker/image/module.rb
@@ -5,35 +5,49 @@ module DockerImageModule
   def docker_image_update
     return false unless docker_check_running
 
-    tag_latest_remote = docker_image_tag_latest_remote
+    begin
+      
+        cmd_docker_pull_latest =
+            config.active['cmd_docker_image_update_docker_pull_latest'] % {
+                docker_user: @docker_user,
+                docker_repo: @docker_repo,
+                tag_latest_remote: 'latest'
+            }
 
-    if tag_latest_remote.to_s.strip.empty?
-      log.error "Unable to get latest remote tag"
-      return false
-    end
+        run_and_exit cmd_docker_pull_latest
 
-    tag_latest_local = docker_image_tag_latest_local
+    rescue RuntimeError
 
-    unless tag_latest_local.to_s.strip.empty?
-      begin
-        if Gem::Version.new(tag_latest_local) >= Gem::Version.new(tag_latest_remote)
-          log.info 'Already up to date.'
-          return true
-        end
-      rescue ArgumentError
-        log.debug "Cannot compare \"#{tag_latest_local}\" and \"#{tag_latest_remote}\""
+      tag_latest_remote = docker_image_tag_latest_remote
+
+      if tag_latest_remote.to_s.strip.empty?
+        log.error "Unable to get latest remote tag"
+        return false
       end
+
+      tag_latest_local = docker_image_tag_latest_local
+
+      unless tag_latest_local.to_s.strip.empty?
+        begin
+          if Gem::Version.new(tag_latest_local) >= Gem::Version.new(tag_latest_remote)
+            log.info 'Already up to date.'
+            return true
+          end
+        rescue ArgumentError
+          log.debug "Cannot compare \"#{tag_latest_local}\" and \"#{tag_latest_remote}\""
+        end
+      end
+
+      log.info "Updating to docker image \"#{@docker_user}/#{@docker_repo}:#{tag_latest_remote}\""
+
+      cmd_docker_pull_latest =
+          config.active['cmd_docker_image_update_docker_pull_latest'] % {
+              docker_user: @docker_user,
+              docker_repo: @docker_repo,
+              tag_latest_remote: tag_latest_remote
+          }
+
+      run_and_exit cmd_docker_pull_latest
     end
-
-    log.info "Updating to docker image \"#{@docker_user}/#{@docker_repo}:#{tag_latest_remote}\""
-
-    cmd_docker_pull_latest =
-        config.active['cmd_docker_image_update_docker_pull_latest'] % {
-            docker_user: @docker_user,
-            docker_repo: @docker_repo,
-            tag_latest_remote: tag_latest_remote
-        }
-
-    run_and_exit cmd_docker_pull_latest
   end
 end
