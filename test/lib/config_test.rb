5e5966e6709a6a00ca7bb99413cefeb336dc12db
Stephan Wehrheim <swehrheim@geospin.de>
2020-10-03 19:55:17 +0200

    Add config unit tests

--- test/lib/config_test.rb
+++ test/lib/config_test.rb
@@ -10,11 +10,36 @@ class ConfigTest < Minitest::Test
     @config.initialize_config
   end
 
-  def test_detect_configured_key
+  def test_that_it_detects_configured_key
+    @config.initialize_config
     assert @config.configured? %w[info_project_main]
   end
 
-  def test_detect_unconfigured_key
+  def test_that_it_detects_unconfigured_key
+    @config.initialize_config
     refute @config.configured? %w[unconfigured_key]
+  end
+
+  def test_that_config_home_beats_default
+    @config.stub :_config_read_home, {'info_project_main' => 'banana'} do
+      @config.initialize_config
+      assert_equal 'banana', @config.config.active['info_project_main']
+    end
+  end
+
+  def test_that_config_project_beats_default
+    @config.stub :_config_read_project, {'info_project_main' => 'tomato'} do
+      @config.initialize_config
+      assert_equal 'tomato', @config.config.active['info_project_main']
+    end
+  end
+
+  def test_that_config_project_beats_home
+    @config.stub :_config_read_home, {'info_project_main' => 'banana'} do
+      @config.stub :_config_read_project, {'info_project_main' => 'tomato'} do
+        @config.initialize_config
+        assert_equal 'tomato', @config.config.active['info_project_main']
+      end
+    end
   end
 end
