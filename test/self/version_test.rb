97aa05146cf942ab7e2c3a4296113c0620afc2a4
Stephan Wehrheim <swehrheim@geospin.de>
2020-10-03 15:16:21 +0200

    test tau version and tau list

--- test/self/version_test.rb
+++ test/self/version_test.rb
@@ -1,11 +1,7 @@
 require "test_helper"
 
-class TakelageTest < Minitest::Test
+class SelfVersionest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Takelage::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
