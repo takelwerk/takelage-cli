a4592b221971edcc1fe265014a1ed59c5f9d540a
Stephan Wehrheim <swehrheim@geospin.de>
2020-10-07 10:22:24 +0200

    Refactor unit test

--- test/self/version_test.rb
+++ test/self/version_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class SelfVersionest < Minitest::Test
+class SelfVersionTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Takelage::VERSION
   end
