2671b15ab3f88fd1cd8b61138784b17e5ee314ef
Stephan Wehrheim <swehrheim@geospin.de>
2020-12-02 15:13:10 +0100

    Satisfy linter

--- lib/takelage/lib/subcmd.rb
+++ lib/takelage/lib/subcmd.rb
@@ -3,9 +3,11 @@
 # Thor with subcommands that work correctly with help
 class SubCommandBase < Thor
   # Set the subcommand banner.
+  # rubocop:disable Style/OptionalBooleanParameter
   def self.banner(command, _namespace = nil, _subcommand = false)
     "#{basename} #{subcommand_prefix} #{command.usage}"
   end
+  # rubocop:enable Style/OptionalBooleanParameter
 
   # Set the subcommand prefix.
   def self.subcommand_prefix
