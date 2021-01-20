1369feb5d7d1ae947ec9d6c2ebb333068d1a7b4d
Stephan Wehrheim <swehrheim@geospin.de>
2021-01-20 15:23:01 +0100

    Raise timeout

--- features/cucumber/support/aruba.rb
+++ features/cucumber/support/aruba.rb
@@ -4,5 +4,5 @@ Aruba.configure do |config|
   config.home_directory = '/tmp/fake_home'
   config.working_directory = '../tmp/cucumber'
   config.activate_announcer_on_command_failure = %i[stderr stdout command]
-  config.exit_timeout = 90
+  config.exit_timeout = 120
 end
