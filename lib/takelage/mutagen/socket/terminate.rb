64ffb404ad7a43d8f5ce2adb6c9b1f858c87d059
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-18 03:22:38 +0100

    Satisfy linter

--- lib/takelage/mutagen/socket/terminate.rb
+++ lib/takelage/mutagen/socket/terminate.rb
@@ -7,10 +7,9 @@ module MutagenSocketTerminate
     log.debug "Terminate the mutagen socket \"#{socket}\""
 
     socket_terminated = _mutagen_socket_terminate_socket socket
-    result = socket_terminated.exitstatus
 
     cmd_remove = config.active['cmd_mutagen_forward_socket_remove']
-    docker_container_command cmd_remove if cmd_remove && result.zero?
+    docker_container_command cmd_remove if cmd_remove && socket_terminated.exitstatus.zero?
 
     unless result.zero?
       log.debug "Unable to terminated mutagen socket \"#{socket}\""
