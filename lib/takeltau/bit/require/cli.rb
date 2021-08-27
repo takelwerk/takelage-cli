7255d849813e914d8150398aa8f5ad357e6cc0f0
Stephan <stephan@takelwerk.net>
2021-08-27 13:31:44 +0200

    Fix typo

--- lib/takeltau/bit/require/cli.rb
+++ lib/takeltau/bit/require/cli.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Takeltau
-  # takeltau bit requiere
+  # takeltau bit require
   class BitRequire < SubCommandBase
     include LoggingModule
     include ConfigModule
