bcf5744a7f5f1597812ae9e8ae3041be7c9befd1
Stephan <stephan@takelwerk.net>
2021-10-22 23:27:30 +0200

    Fix unit tests

--- test/self/commands_test.rb
+++ test/self/commands_test.rb
@@ -2,32 +2,32 @@
 
 require 'test_helper'
 
-class SelfListTest < Minitest::Test
+class SelfCommandsTest < Minitest::Test
   # rubocop:disable Metrics/MethodLength
   def setup
     @thor_list = %(
       takeltau
       --------
       thor self help [COMMAND]                          # Describe subcommands or one specific subcommand
-      thor self list                                    # List all commands
+      thor self commands                                # List all commands
       thor self version                                 # Print takeltau semantic version number
       thor takeltau:c_l_i:self [COMMAND]                # Manage takeltau tools
       thor takeltau:c_l_i:version                       # Alias for tau self version
     )
     @expected_output = %(
-      tau self list                                    # List all commands
+      tau self commands                                # List all commands
       tau self version                                 # Print takeltau semantic version number
       tau version                                      # Alias for tau self version
     )
-    @self_list = Object.new
-    @self_list.extend(SelfList)
+    @self_commands = Object.new
+    @self_commands.extend(SelfCommands)
   end
   # rubocop:enable Metrics/MethodLength
 
   def test_that_it_manipulates_output_of_thor_list
-    @self_list.stub :_get_thor_list_, @thor_list do
-      list_output = @self_list.self_list
-      assert_equal @expected_output, list_output
+    @self_commands.stub :_get_thor_list_, @thor_list do
+      commands_output = @self_commands.self_commands
+      assert_equal @expected_output, commands_output
     end
   end
 end
