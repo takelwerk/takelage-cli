9e3cfbe3845eb332cfcecad3e6f694cacff8455c
Stephan <stephan@takelwerk.net>
2024-11-07 19:01:09 +0100

    Refactor ship restart

--- lib/takeltau/ship/project/restart.rb
+++ lib/takeltau/ship/project/restart.rb
@@ -5,20 +5,7 @@ module ShipProjectRestart
   # Restart a takelship
   def ship_project_restart(project)
     say ship_container_stop
-    _ship_project_restart_wait
-    ship_project_start project
-  end
-
-  private
-
-  # Wait until a takelship container is stopped
-  def _ship_project_restart_wait
-    cmd_ship_docker_wait = format(
-      config.active['cmd_ship_docker_wait'],
-      ship_docker: config.active['cmd_ship_docker'],
-      ship_hostname: _ship_container_lib_ship_hostname
-    )
-
-    run cmd_ship_docker_wait
+    sleep 1 until ship_project_start project, mute: true
+    true
   end
 end
