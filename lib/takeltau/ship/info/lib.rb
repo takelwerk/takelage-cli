6517807e034aaa7ac393db4e46a732b7553bb51a
Stephan <stephan@takelwerk.net>
2024-11-08 11:23:34 +0100

    Add more type checks

--- lib/takeltau/ship/info/lib.rb
+++ lib/takeltau/ship/info/lib.rb
@@ -23,7 +23,7 @@ module ShipInfoLib
   def _ship_info_lib_get_project(project, takelship)
     return '' if takelship.nil?
 
-    return '' unless takelship.class == Hash
+    return '' unless takelship.instance_of?(Hash)
 
     return '' unless takelship.key? 'default_project'
 
