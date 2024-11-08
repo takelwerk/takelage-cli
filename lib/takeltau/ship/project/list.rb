735ea200f4868acdab6540e50172f28c4745bdc7
Stephan <stephan@takelwerk.net>
2024-11-08 15:21:11 +0100

    Remove nil check

--- lib/takeltau/ship/project/list.rb
+++ lib/takeltau/ship/project/list.rb
@@ -5,7 +5,20 @@ module ShipProjectList
   # List takelship projects
   def ship_project_list
     log.debug 'List takelship projects'
-    _ship_info_lib_get_takelshipinfo['projects'].each do |project|
+    takelship = _ship_info_lib_get_takelshipinfo
+    unless takelship.instance_of?(Hash)
+      say 'Could not get takelship info'
+      say 'Try: ship list --debug'
+      return false
+    end
+
+    unless takelship.key?('projects')
+      say 'Could not get takelship projects'
+      say 'Try: ship list --debug'
+      return false
+    end
+
+    takelship['projects'].each do |project|
       say project['name']
     end
   end
